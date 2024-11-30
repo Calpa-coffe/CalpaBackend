6b9907046e0f0947b88f7c493e43cc7fd50e49a1
Educando23 <u20211g019@upc.edu.pe>
2024-11-30 02:43:09 -0500

    Commit repository user

---
+++ src/main/java/pe/edu/upc/calpabackend/repositories/IUserRepository.java
@@ -0,0 +1,11 @@
+package pe.edu.upc.calpabackend.repositories;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.edu.upc.calpabackend.entities.Users;
+
+@Repository
+public interface IUserRepository extends JpaRepository<Users, Integer> {
+}
