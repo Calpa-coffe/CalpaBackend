3ca36939d285114073a1ad394c326a4ef3e4a46d
Joaquin Rivera <joaquinrivram@arizona.edu>
2024-11-30 02:07:09 -0500

    Initial commit

---
+++ src/test/java/pe/edu/upc/calpabackend/CalpaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.calpabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalpaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
